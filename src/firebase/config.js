10bf5b65a4ceb7449468de323f21356dc3800789
Harish Joshi <dev.harishjoshi@gmail.com>
2021-12-31 04:11:30 +0545

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// firebase version 7.16.1
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: '',
+	authDomain: '',
+	projectId: '',
+	storageBucket: '',
+	messagingSenderId: '',
+	appId: '',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
